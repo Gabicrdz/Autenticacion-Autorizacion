a21adc2b8f15853c0159d2d1c25ef270495ebbc7
Gabicrdz <cardozosg2003@gmail.com>
2024-09-06 16:24:37 -0300

    backend y fronted funcionando

---
+++ backend-jwt/app.js
@@ -0,0 +1,35 @@
+// server.js
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import cors from 'cors';
+import morgan from 'morgan';
+
+import { PORT } from './config/env.js';
+import authRouter from './routes/auth.routes.js';
+
+
+
+const app = express();
+
+app.use(cors({
+    origin: ['http://localhost:5500', 'http://localhost:3000', 'http://127.0.0.1:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cookieParser());
+app.use(session({
+    secret: 'session_secret_key', // Cambia esto por una clave secreta en producción
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // Usar 'true' si usas HTTPS
+}));
+
+app.use(authRouter)
+
+// Servidor escuchando
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
